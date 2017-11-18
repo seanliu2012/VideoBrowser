399a9271aee7097156d8d3c0e6785384ffa95e87
Sean Liu <sean.liu@myob.com>
2017-11-19 10:33:37 +1100

    add youtube-api-search package and set up search_bar the basic component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reducers from "./reducers";
 
+import SearchBar from "./components/search_bar";
+
 const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+//const API_KEY = 'AIzaSyBaTOnWpE3cjsynjrlisBE9dGeYm7xDN9s';
+
+// testing App component
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+}
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
