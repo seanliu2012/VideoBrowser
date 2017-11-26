9b9563a40a20af04a8025e74b768430a19d88833
Sean Liu <sean.liu@myob.com>
2017-11-26 11:29:13 +1100

    remove key

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import YTSearch from "youtube-api-search";
 import SearchBar from "./components/search_bar";
 import VideoList from "./components/video_list";
 import VideoDetail from './components/video_detail';
-const API_KEY = 'AIzaSyC1s_gdldQ13p1n-mylDdOhJUzeYZfgcAI';
+const API_KEY = '<youtube-data-API-key>';
 
 
 class App extends Component {
